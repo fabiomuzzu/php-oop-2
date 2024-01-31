7ce4dc372f77368429bcf5f8b679be0870234e7b
fabiomuzzu <fabio.muzzu@virgilio.it>
2024-01-31 20:06:41 +0100

    added print of "articoliCibo" array

--- index.php
+++ index.php
@@ -6,13 +6,15 @@
         public $descrizione;
         public $prezzo;
         public $disponibilita;
+        public $genere;
 
-        public function __construct($_nome, $_immagine, $_descrizione, $_prezzo, $_disponibilita){
+        public function __construct($_nome, $_immagine, $_descrizione, $_prezzo, $_disponibilita, Genere $_genere){
             $this->nome = $_nome;
             $this->immagine = $_immagine;
             $this->descrizione = $_descrizione;
             $this->prezzo = $_prezzo;
             $this->disponibilita = $_disponibilita;
+            $this->genere = $_genere;
         }
     }
 
@@ -29,8 +31,8 @@
     class Cibo extends Prodotti{
         public $proteine;
 
-        function __construct($_nome, $_immagine, $_descrizione, $_prezzo, $_disponibilita, $_proteine){
-            parent::__construct($_nome, $_immagine, $_descrizione, $_prezzo, $_disponibilita);
+        function __construct($_nome, $_immagine, $_descrizione, $_prezzo, $_disponibilita, $_genere, $_proteine){
+            parent::__construct($_nome, $_immagine, $_descrizione, $_prezzo, $_disponibilita, $_genere);
             $this->proteine = $_proteine;
         }
     }
@@ -39,8 +41,8 @@
     class Giochi extends Prodotti{
         public $materiale;
 
-        function __construct($_nome, $_immagine, $_descrizione, $_prezzo, $_disponibilita, $_materiale){
-            parent::__construct($_nome, $_immagine, $_descrizione, $_prezzo, $_disponibilita);
+        function __construct($_nome, $_immagine, $_descrizione, $_prezzo, $_disponibilita, $_genere, $_materiale){
+            parent::__construct($_nome, $_immagine, $_descrizione, $_prezzo, $_disponibilita, $_genere);
             $this->materiale = $_materiale;
         }
     }
@@ -49,34 +51,34 @@
     class Accessori extends Prodotti{
         public $necessita;
 
-        function __construct($_nome, $_immagine, $_descrizione, $_prezzo, $_disponibilita, $_necessita){
-            parent::__construct($_nome, $_immagine, $_descrizione, $_prezzo, $_disponibilita);
+        function __construct($_nome, $_immagine, $_descrizione, $_prezzo, $_disponibilita,$_genere, $_necessita){
+            parent::__construct($_nome, $_immagine, $_descrizione, $_prezzo, $_disponibilita, $_genere);
             $this->necessita = $_necessita;
         }
     }
 
     // Definizione dei generi di animale
-    $cane = new Genere('cane');
-    $gatto = new Genere('gatto');
-    $pesce = new Genere('pesce');
-    $uccello = new Genere('uccello');
+    $cane = new Genere('Cane');
+    $gatto = new Genere('Gatto');
+    $pesce = new Genere('Pesce');
+    $uccello = new Genere('Uccello');
 
     $articoliCibo = 
     [
-        new Cibo('Royal Canin Mini Adult', 'https://arcaplanet.vtexassets.com/arquivos/ids/284621/Mini-Adult.jpg?v=638182891693570000', 'Crocchette energetiche', 5, 'disponibile', $cane, '1200'),
-        new Cibo('Almo Nature Holistic Maintenance Medium Large Tonno e Riso', 'https://arcaplanet.vtexassets.com/arquivos/ids/245173/almo-nature-holistic-cane-adult-medium-pollo-e-riso.jpg', 'Crocchette tonno e riso', 30, 'disponibile', $cane, '1200'),
-        new Cibo('Almo Nature Cat Daily Lattina', 'https://arcaplanet.vtexassets.com/arquivos/ids/245336/almo-daily-menu-cat-400-gr-vitello.jpg', 'Bocconcini con vitello', 3, 'disponibile', $gatto, '1200'),
-        new Cibo('Mangime per Pesci Guppy in Fiocchi', 'https://arcaplanet.vtexassets.com/arquivos/ids/272714/tetra-guppy-mini-flakes.jpg', 'Mangime per pesci', 7, 'non disponibile', $pesce, '1200'),
+        new Cibo('Royal Canin Mini Adult', 'https://arcaplanet.vtexassets.com/arquivos/ids/284621/Mini-Adult.jpg?v=638182891693570000', 'Crocchette energetiche', 5, 'Disponibile', $cane, '1200'),
+        new Cibo('Almo Nature Holistic Maintenance Medium Large Tonno e Riso', 'https://arcaplanet.vtexassets.com/arquivos/ids/245173/almo-nature-holistic-cane-adult-medium-pollo-e-riso.jpg', 'Crocchette tonno e riso', 30, 'Disponibile', $cane, '1200'),
+        new Cibo('Almo Nature Cat Daily Lattina', 'https://arcaplanet.vtexassets.com/arquivos/ids/245336/almo-daily-menu-cat-400-gr-vitello.jpg', 'Bocconcini con vitello', 3, 'Disponibile', $gatto, '1200'),
+        new Cibo('Mangime per Pesci Guppy in Fiocchi', 'https://arcaplanet.vtexassets.com/arquivos/ids/272714/tetra-guppy-mini-flakes.jpg', 'Mangime per pesci', 7, 'Non disponibile', $pesce, '1200'),
     ];
     $articoliGiochi = 
     [
-        new Giochi('Kong Classic', 'https://arcaplanet.vtexassets.com/arquivos/ids/256599/kong-classic1.jpg', 'Giocattolo per cani', 10, 'non disponibile', $cane, 'Gomma'),
-        new Giochi('Topini di peluche Trixie', 'https://arcaplanet.vtexassets.com/arquivos/ids/223852/trixie-gatto-gioco-active-mouse-peluche.jpg', 'Giocattolo per gatti', 7, 'disponibile', $gatto, 'Plastica'),
+        new Giochi('Kong Classic', 'https://arcaplanet.vtexassets.com/arquivos/ids/256599/kong-classic1.jpg', 'Giocattolo per cani', 10, 'Non disponibile', $cane, 'Gomma'),
+        new Giochi('Topini di peluche Trixie', 'https://arcaplanet.vtexassets.com/arquivos/ids/223852/trixie-gatto-gioco-active-mouse-peluche.jpg', 'Giocattolo per gatti', 7, 'Disponibile', $gatto, 'Plastica'),
     ];
     $articoliAccessori = 
     [
-        new Accessori('Voliera Wilma in Legno', 'https://arcaplanet.vtexassets.com/arquivos/ids/258384/voliera-wilma1.jpg', 'Voliera per uccelli', 3, 'non disponibile', $uccello, 'Alta'),
-        new Accessori('Cartucce Filtranti per Filtro EasyCrystal', 'https://arcaplanet.vtexassets.com/arquivos/ids/272741/tetra-easycrystal-filterpack-250-300.jpg', 'Filtro per acquari', 19, 'disponibile', $pesce, 'Obbligatoria la sostiutizone ogni 3 mesi'),
+        new Accessori('Voliera Wilma in Legno', 'https://arcaplanet.vtexassets.com/arquivos/ids/258384/voliera-wilma1.jpg', 'Voliera per uccelli', 3, 'Non disponibile', $uccello, 'Alta'),
+        new Accessori('Cartucce Filtranti per Filtro EasyCrystal', 'https://arcaplanet.vtexassets.com/arquivos/ids/272741/tetra-easycrystal-filterpack-250-300.jpg', 'Filtro per acquari', 19, 'Disponibile', $pesce, 'Obbligatoria la sostiutizone ogni 3 mesi'),
     ];
 
 ?>
@@ -92,22 +94,23 @@
 <body>
     <div class="container">
         <div class="row">
-            <div class="card" style="width: 18rem;">
-                <img src="..." class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <?php foreach($articoliCibo as $item) { ?>
+                <div class="col-3">
+                    <div class="card" style="width: 18rem;">
+                        <img src="<?php echo $item->immagine ?>" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title"><?php echo $item->nome ?></h5>
+                            <p class="card-text"><?php echo $item->descrizione ?></p>
+                        </div>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item"><?php echo '€'.$item->prezzo ?></li>
+                            <li class="list-group-item"><?php echo $item->genere->genere ?></li>
+                            <li class="list-group-item"><?php echo $item->disponibilita ?></li>
+                            <li class="list-group-item"><?php echo $item->proteine ?></li>
+                        </ul>
+                    </div>
                 </div>
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item">An item</li>
-                    <li class="list-group-item">A second item</li>
-                    <li class="list-group-item">A third item</li>
-                </ul>
-                <div class="card-body">
-                    <a href="#" class="card-link">Card link</a>
-                    <a href="#" class="card-link">Another link</a>
-                </div>
-            </div>
+            <?php } ?>
         </div>
     </div>
 </body>
