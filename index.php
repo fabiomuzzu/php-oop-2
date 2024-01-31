7666fe4c83713e7ee911c0f364a2462a343a7e07
fabiomuzzu <fabio.muzzu@virgilio.it>
2024-01-31 19:00:59 +0100

    added Genere and other property inside the array

--- index.php
+++ index.php
@@ -27,12 +27,10 @@
 
     // Definizione classe Cibi ereditiera della classe Prodotti 
     class Cibo extends Prodotti{
-        public $ingredienti;
         public $proteine;
 
-        public function __construct($_nome, $_immagine, $_descrizione, $_prezzo, $_disponibilita, $ingredienti, $proteine){
+        public function __construct($_nome, $_immagine, $_descrizione, $_prezzo, $_disponibilita, $proteine){
             parent::__construct($_nome, $_immagine, $_descrizione, $_prezzo, $_disponibilita);
-            $this->ingredienti = $_ingredienti;
             $this->proteine = $_proteine;
         }
     }
@@ -40,12 +38,10 @@
     // Definizione classe Giochi ereditiera della classe Prodotti 
     class Giochi extends Prodotti{
         public $materiale;
-        public $dimensione;
 
-        public function __construct($_nome, $_immagine, $_descrizione, $_prezzo, $_disponibilita, $materiale, $dimensione){
+        public function __construct($_nome, $_immagine, $_descrizione, $_prezzo, $_disponibilita, $materiale){
             parent::__construct($_nome, $_immagine, $_descrizione, $_prezzo, $_disponibilita);
             $this->materiale = $_materiale;
-            $this->dimensione = $_dimensione;
         }
     }
 
@@ -67,20 +63,20 @@
 
     $articoliCibo = 
     [
-        new Cibo('Royal Canin Mini Adult', 'https://arcaplanet.vtexassets.com/arquivos/ids/284621/Mini-Adult.jpg?v=638182891693570000', 'Crocchette energetiche', 5, 'disponibile'),
-        new Cibo('Almo Nature Holistic Maintenance Medium Large Tonno e Riso', 'https://arcaplanet.vtexassets.com/arquivos/ids/245173/almo-nature-holistic-cane-adult-medium-pollo-e-riso.jpg', 'Crocchette tonno e riso', 30, 'disponibile'),
-        new Cibo('Almo Nature Cat Daily Lattina', 'https://arcaplanet.vtexassets.com/arquivos/ids/245336/almo-daily-menu-cat-400-gr-vitello.jpg', 'Bocconcini con vitello', 3, 'disponibile'),
-        new Cibo('Mangime per Pesci Guppy in Fiocchi', 'https://arcaplanet.vtexassets.com/arquivos/ids/272714/tetra-guppy-mini-flakes.jpg', 'Mangime per pesci', 7, 'non disponibile'),
+        new Cibo('Royal Canin Mini Adult', 'https://arcaplanet.vtexassets.com/arquivos/ids/284621/Mini-Adult.jpg?v=638182891693570000', 'Crocchette energetiche', 5, 'disponibile', $cane, '1200'),
+        new Cibo('Almo Nature Holistic Maintenance Medium Large Tonno e Riso', 'https://arcaplanet.vtexassets.com/arquivos/ids/245173/almo-nature-holistic-cane-adult-medium-pollo-e-riso.jpg', 'Crocchette tonno e riso', 30, 'disponibile', $cane, '1200'),
+        new Cibo('Almo Nature Cat Daily Lattina', 'https://arcaplanet.vtexassets.com/arquivos/ids/245336/almo-daily-menu-cat-400-gr-vitello.jpg', 'Bocconcini con vitello', 3, 'disponibile', $gatto, '1200'),
+        new Cibo('Mangime per Pesci Guppy in Fiocchi', 'https://arcaplanet.vtexassets.com/arquivos/ids/272714/tetra-guppy-mini-flakes.jpg', 'Mangime per pesci', 7, 'non disponibile', $pesce, '1200'),
     ];
     $articoliGiochi = 
     [
-        new Giochi('Kong Classic', 'https://arcaplanet.vtexassets.com/arquivos/ids/256599/kong-classic1.jpg', 'Giocattolo per cani', 10, 'non disponibile'),
-        new Giochi('Topini di peluche Trixie', 'https://arcaplanet.vtexassets.com/arquivos/ids/223852/trixie-gatto-gioco-active-mouse-peluche.jpg', 'Giocattolo per gatti', 7, 'disponibile'),
+        new Giochi('Kong Classic', 'https://arcaplanet.vtexassets.com/arquivos/ids/256599/kong-classic1.jpg', 'Giocattolo per cani', 10, 'non disponibile', $cane, 'Gomma'),
+        new Giochi('Topini di peluche Trixie', 'https://arcaplanet.vtexassets.com/arquivos/ids/223852/trixie-gatto-gioco-active-mouse-peluche.jpg', 'Giocattolo per gatti', 7, 'disponibile', $gatto, 'Plastica'),
     ];
     $articoliAccessori = 
     [
-        new Accessori('Voliera Wilma in Legno', 'https://arcaplanet.vtexassets.com/arquivos/ids/258384/voliera-wilma1.jpg', 'Voliera per uccelli', 3, 'non disponibile'),
-        new Accessori('Cartucce Filtranti per Filtro EasyCrystal', 'https://arcaplanet.vtexassets.com/arquivos/ids/272741/tetra-easycrystal-filterpack-250-300.jpg', 'Filtro per acquari', 19, 'disponibile'),
+        new Accessori('Voliera Wilma in Legno', 'https://arcaplanet.vtexassets.com/arquivos/ids/258384/voliera-wilma1.jpg', 'Voliera per uccelli', 3, 'non disponibile', $uccello, 'Alta'),
+        new Accessori('Cartucce Filtranti per Filtro EasyCrystal', 'https://arcaplanet.vtexassets.com/arquivos/ids/272741/tetra-easycrystal-filterpack-250-300.jpg', 'Filtro per acquari', 19, 'disponibile', $pesce, 'Obbligatoria la sostiutizone ogni 3 mesi'),
     ];
 
 ?>
