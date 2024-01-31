82ccbc17cd87c82686b8c0fb797f7ca5840dd65a
fabiomuzzu <fabio.muzzu@virgilio.it>
2024-01-31 16:25:29 +0100

    added product class inside php tag

--- index.php
+++ index.php
@@ -1,9 +1,32 @@
+<?php 
+    // Classse padre dei prodotti
+    class Prodotti{
+        public $nome;
+        public $immagine;
+        public $descrizione;
+        public $prezzo;
+        public $disponibilità;
+
+        public function __construct($_nome, $_immagine, $_descrizione, $_prezzo, $_disponibilità){
+            $this->nome = $_nome;
+            $this->immagine = $_immagine;
+            $this->descrizione = $_descrizione;
+            $this->prezzo = $_prezzo;
+            $this->disponibilità = $_disponibilità;
+        }
+
+        
+    }
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Pet eCommerce</title>
 </head>
 <body>
     
